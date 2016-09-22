6187e93143fec8bea067661cf33a0c198f3026f9
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2016-09-22 12:43:07 +0400

    Run gulp-yoman. Initial commit

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('lobilistAngular')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
